9e17741ab1dd7d3a1913dc82a6c91fde4612b950
Qui Phan <pvqui0910@gmail.com>
2021-09-26 17:03:58 +0700

    add routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+
+import Product from './views/product';
+import Login from './views/login';
+import Home from './views/home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+    <Router>
+      <div>
+        <Switch>
+        <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/product">
+            <Product />
+          </Route>
+          <Route path="/login" render={function () { return <Login /> }} />
+        
+          <PrivateRoute path="/404">
+            <Home />
+          </PrivateRoute>
+
+        </Switch>
+      </div>
+    </Router>
+
+
+  );
+}
+
+function PrivateRoute({ children, ...rest }) {
+  return (
+    <Route {...rest} render={function () {
+      return localStorage.todoApp_accessToken ?
+        children :
+        <Redirect to={{ pathname: '/' }} />;
+    }} />
   );
 }
 
