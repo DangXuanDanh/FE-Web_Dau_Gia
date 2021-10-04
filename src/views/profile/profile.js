5c80a99d210b3436ab8ad4ab1c69f2d994bcdbdc
Đặng Xuân Danh <57908643+DangXuanDanh@users.noreply.github.com>
2021-10-04 19:32:36 +0700

    done show profile

--- src/views/profile/profile.js
+++ src/views/profile/profile.js
@@ -6,110 +6,104 @@ import Alert from "../../common/alert"
 import './profile.css';
 
 
-function Profile () {
+function Profile() {
+    const saved = localStorage.getItem('user');;
+
+    const history = useHistory();
+
+    const [profile, setprofile] = useState([]);
     const [showProfile, setShowProfile] = useState(false);
-    const onShowProfile = (e) => {
-        showProfileModal();
-    };
+    const [id, setId] = useState([]);
+    const [name, setName] = useState([]);
+    const [email, setEmail] = useState([]);
+    const [date, setDate] = useState([]);
+    const [diachi, setDiaChi] = useState([]);
+    const [transactions, setTransactions] = useState([]);
+    const [transactionsProgram, setTransactionsProgram] = useState([]);
 
-    const showProfileModal = () => {
-        setShowProfile(true);
-    };
+    const [errorMessage, setErrorMessage] = useState('');
+    const [alertStatus, setAlertStatus] = useState(false);
+    const [alertType, setAlertType] = useState('');
+
+    useEffect(async () => {
+        if (!localStorage.getItem('user')) {
+            history.push('/');
+        }
+        
+        else {
+            const initial = JSON.parse(saved);
+            setId(initial.mataikhoan);
+            await fetch('http://localhost:3000/API/user/profile/' + initial.mataikhoan, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+            }).then(async function (response) {
+                const result = await response.json();
+                setprofile(result);
+                setName(result.hoten);
+                setEmail(result.email);
+                setDate(result.ngaysinh);
+                setDiaChi(result.diachi);
+
+            }).catch((error) => {
+                return error;
+            });
+
+        }
+    }, []);
+
+
     return (
-            <div className="mt-3">
-                <div className="profile">
-                    <div className="d-flex justify-content-between align-items-center mb-3">
-                        <h3 className="w-100 bold">Thông Tin Tài Khoản</h3>
-                    </div>
-                    <div className="card-body text-left">
-                        <div className="row">
-                            <div className="col-sm-3 vertical-base">
-                                <h6 className="mb-0 float-left">Họ Tên</h6>
-                            </div>
-                            <div className="col-sm-9 text-secondary">
-                                Danh
-                            </div>
+        <div className="mt-3">
+            <div className="profile">
+                <div className="d-flex justify-content-between align-items-center mb-3">
+                    <h3 className="w-100 bold">Thông Tin Tài Khoản</h3>
+                </div>
+                <div className="card-body text-left">
+                    <div className="row mb-3">
+                        <div className="col-sm-3 vertical-base">
+                            <h6 className="mb-0 float-left">Họ Tên</h6>
                         </div>
-
-                        <div className="row">
-                            <div className="col-sm-3 vertical-base">
-                                <h6 className="mb-0 float-left">Email</h6>
-                            </div>
-                            <div className="col-sm-9 text-secondary">
-                                asd@gmail.com
-                            </div>
-                        </div>
-
-                        <div className="row">
-                            <div className="col-sm-3 vertical-base">
-                                <h6 className="mb-0 float-left">Ngày Sinh</h6>
-                            </div>
-                            <div className="col-sm-9 text-secondary">
-                                14/01/1999
-                            </div>
-                        </div>
-
-                        <div className="row">
-                            <div className="col-sm-3 vertical-base">
-                                <h6 className="mb-0 float-left">Địa Chỉ</h6>
-                            </div>
-                            <div className="col-sm-9 text-secondary">
-                                74/9 asdsad asdf
-                            </div>
+                        <div className="col-sm-9 text-secondary">
+                            {name}
                         </div>
                     </div>
 
-                    <div>
-                        <Button className="mt-3 btn btn-default text-white btn-lg" onClick={onShowProfile}>Thay
-                            Đổi</Button>
+                    <div className="row mb-3">
+                        <div className="col-sm-3 vertical-base">
+                            <h6 className="mb-0 float-left">Email</h6>
+                        </div>
+                        <div className="col-sm-9 text-secondary">
+                            {email}
+                        </div>
+                    </div>
+
+                    <div className="row mb-3">
+                        <div className="col-sm-3 vertical-base">
+                            <h6 className="mb-0 float-left">Ngày Sinh</h6>
+                        </div>
+                        <div className="col-sm-9 text-secondary">
+                            {date}
+                        </div>
+                    </div>
+
+                    <div className="row mb-3">
+                        <div className="col-sm-3 vertical-base">
+                            <h6 className="mb-0 float-left">Địa Chỉ</h6>
+                        </div>
+                        <div className="col-sm-9 text-secondary">
+                            {diachi}
+                        </div>
                     </div>
                 </div>
 
-            <Modal
-                className="details_modal"
-                show={showProfile}
-                onHide={false}
-                keyboard={false}
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title><div className="float-left">Thay Đổi Thông Tin</div></Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Form className="mt-1">
-                        <Form.Group className="mb-3" controlId="formBasicName">
-                            <Form.Label className="float-left">Họ Tên</Form.Label>
-                            <Form.Control type="text"/>
-                        </Form.Group>
-
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label className="float-left">Email</Form.Label>
-                            <Form.Control type="email"/>
-                        </Form.Group>
-
-                        <Form.Group className="mb-3" controlId="formBasicPhone">
-                            <Form.Label className="float-left">Điện Thoại</Form.Label>
-                            <Form.Control type="number"/>
-                        </Form.Group>
-
-                        <Form.Group className="mb-3" controlId="formBasicCardId">
-                            <Form.Label className="float-left">Chứng Minh Nhân Dân</Form.Label>
-                            <Form.Control type="number"/>
-                        </Form.Group>
-                    </Form>
-
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button className="btn btn-primary" variant="primary">
-                        Lưu
-                    </Button>
-
-                    <Button variant="danger" onClick={false}>
-                        Hủy
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-
-        
+                <div>
+                    <Button className="mt-3 btn btn-default text-white btn-lg">Thay
+                        Đổi</Button>
+                </div>
+            </div>
         </div>
     );
 }
