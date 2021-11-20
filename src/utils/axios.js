92d6b28de4c5e47ec2364d322d51fa61eb378938
phglong1999 <56074254+phglong1999@users.noreply.github.com>
2021-11-20 23:02:20 +0700

    thay đổi port

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
   // baseURL: `http://localhost:${process.env.PORT}/api/`,
-  baseURL: `http://localhost:3000/api/`,
+  baseURL: `http://localhost:3001/api/`,
   timeout: 25000,
   // headers: {
   //   'X-Access-Token': 'accessToken'
